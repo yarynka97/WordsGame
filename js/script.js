4989355c89e56edcc4e435fa3d7d77d4bc3a90c4
Yarynka Shyika <pinkpoppy.art@gmail.com>
2018-02-05 16:29:32 +0200

    first functionality is added

---
+++ js/script.js
@@ -0,0 +1,47 @@
+﻿$(function () {
+    var usedWords = [];
+    var commonUrl = "https://api.datamuse.com/words?sp=";
+    var urlToSend;
+    setFirstWord();
+    $("button").click(function () {
+        var usersWord = $("input").val();
+        usedWords.push(usersWord);
+        $("#words-field").append(" - " + usersWord);
+        var lastLetter = usersWord[usersWord.length - 1];
+        urlToSend = commonUrl + lastLetter + "*";
+        $("#bot-answer").text(urlToSend);
+        $.ajax({
+            type: "GET",
+            url: urlToSend,
+            success: function (words) {
+                var word = words[parseInt(Math.random() * words.length)].word;
+                usedWords.push(word);
+                $("#words-field").append(" - " + word);
+                alert(usedWords);
+            }
+        });
+        console.log("click");
+    });
+
+    function setFirstWord() {
+        $("#bot-answer").addClass("bot-answer-ok");
+        var randomLetter = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 1);
+        urlToSend = commonUrl + randomLetter + "*";
+        $.ajax({
+            type: "GET",
+            url: urlToSend,
+            success: function (words) {
+                var word = words[parseInt(Math.random() * words.length)].word;
+                usedWords.push(word);
+                $("#bot-answer").text("My word is " + word);
+                $("#words-field").append(word);
+            }
+        });
+    }
+    //function mistake(errorText) {
+    //    $("#bot-answer")
+    //        .removeClass("bot-answer-ok")
+    //        .addClass("bot-answer-mistake")
+    //        .text(errorText);
+    //}
+});
